dc053b5a0d63a6fd362292ed96299816cd503f7d
Rangel Reale <rangelspam@gmail.com>
2023-07-04 17:56:34 -0300

    cleanup

--- alias.go
+++ alias.go
@@ -26,6 +26,14 @@ type (
 // MapTags is an alternative to struct tags, and can be used to override them.
 type MapTags = instruct.MapTags
 
+// option_field.go
+
+// StructOption can be used as a struct field to give options to the struct itself.
+type StructOption = instruct.StructOption
+
+// StructOptionMapTag corresponds to StructOption in a MapTags.
+const StructOptionMapTag = instruct.StructOptionMapTag
+
 // resolver.go
 
 // Resolver converts strings to the type of the struct field.
--- option_field.go
+++
@@ -1,9 +0,0 @@
-package inreq
-
-import "github.com/RangelReale/instruct"
-
-// StructOption can be used as a struct field to give options to the struct itself.
-type StructOption = instruct.StructOption
-
-// StructOptionMapTag corresponds to StructOption in a MapTags.
-const StructOptionMapTag = instruct.StructOptionMapTag
