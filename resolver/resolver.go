473a9be915e89ae4e90fd771dc41ce3831f5c018
Rangel Reale <rangelspam@gmail.com>
2023-07-09 10:17:34 -0300

    resolver alias

---
+++ resolver/resolver.go
@@ -0,0 +1,8 @@
+package resolver
+
+import (
+	"github.com/RangelReale/instruct/resolver"
+)
+
+// Resolver is the default Resolver.
+type Resolver = resolver.Resolver
---
+++ resolver/resolver_value.go
@@ -0,0 +1,17 @@
+package resolver
+
+import (
+	"github.com/RangelReale/instruct/resolver"
+)
+
+// ValueResolver resolves simple types for a Resolver.
+// It should NOT handle slices, pointers, or maps.
+type ValueResolver resolver.ValueResolver
+
+// TypeValueResolver is a custom type handler for a ValueResolver.
+// It should NOT process value using reflection (for performance reasons).
+type TypeValueResolver = resolver.TypeValueResolver
+
+// TypeValueResolverReflect is a custom type handler for a ValueResolver.
+// It SHOULD process value using reflection.
+type TypeValueResolverReflect = resolver.TypeValueResolverReflect
